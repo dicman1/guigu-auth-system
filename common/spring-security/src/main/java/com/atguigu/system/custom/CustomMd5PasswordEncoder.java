b46f85720793d26edbbd8ce33202f6353de46fb4
liyuang <dicyuang@163.com>
2022-11-23 20:48:38 +0800

    commit final -g

--- common/spring-security/src/main/java/com/atguigu/system/custom/CustomMd5PasswordEncoder.java
+++ common/spring-security/src/main/java/com/atguigu/system/custom/CustomMd5PasswordEncoder.java
@@ -2,21 +2,20 @@ package com.atguigu.system.custom;
 import com.atguigu.common.utils.MD5;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
 /**
  * <p>
- * 密码处理
+ * 密码处理方法
  * </p>
  *
  */
 @Component
 public class CustomMd5PasswordEncoder implements PasswordEncoder {
 
-    public String encode(CharSequence rawPassword) {
+    public String encode(CharSequence rawPassword){
         return MD5.encrypt(rawPassword.toString());
     }
 
-    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+    public boolean matches(CharSequence rawPassword, String encodedPassword){
         return encodedPassword.equals(MD5.encrypt(rawPassword.toString()));
     }
 }
